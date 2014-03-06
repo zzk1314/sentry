ab5c4d9f24c4a6341a479e881f44d3ed289c7ccb
David Cramer <dcramer@gmail.com>
2014-03-06 13:45:34 -0800

    Form saving on team settings

--- src/sentry/static/sentry/app/routes/manageTeamSettings.js
+++ src/sentry/static/sentry/app/routes/manageTeamSettings.js
@@ -5,17 +5,29 @@ define(['app', 'angular'], function(app, angular) {
         parent: 'manage_team',
         url: 'settings/',
         templateUrl: 'partials/manage-team-settings.html',
-        controller: function($scope, selectedTeam){
+        controller: function($scope, $http, selectedTeam){
             $scope.teamData = angular.copy(selectedTeam);
 
-            $scope.saveField = function($event) {
-                var $element = angular.element($event.target);
-                var $model = $element.controller('ngModel');
-                var $name = $model.$name;
-                var $value = $model.$modelValue;
+            // $scope.saveField = function($event) {
+            //     var $element = angular.element($event.target);
+            //     var $model = $element.controller('ngModel');
+            //     var $name = $model.$name;
+            //     var $value = $model.$modelValue;
 
-                selectedTeam[$name] = $value;
-                // TOOD: send to server
+            //     selectedTeam[$name] = $value;
+            //     // TOOD: send to server
+            // };
+
+            $scope.isUnchanged = function(team) {
+                return angular.equals(team, selectedTeam);
+            };
+
+            $scope.saveForm = function() {
+                $http.put('/api/0/teams/' + $scope.teamData.id + '/', $scope.teamData)
+                    .success(function(data){
+                        $scope.teamData = data;
+                        selectedTeam = data;
+                    });
             };
         }
     };
