a98bf597a620dbca53ca092ddfbf228c3bc30917
David Cramer <dcramer@gmail.com>
2014-03-06 12:39:43 -0800

    Initial work on team settings form

--- src/sentry/static/sentry/app/routes/manageTeamSettings.js
+++ src/sentry/static/sentry/app/routes/manageTeamSettings.js
@@ -1,11 +1,22 @@
-define(['app'], function(app) {
+define(['app', 'angular'], function(app, angular) {
     'use strict';
 
     return {
         parent: 'manage_team',
         url: 'settings/',
         templateUrl: 'partials/manage-team-settings.html',
-        controller: function($scope){
+        controller: function($scope, selectedTeam){
+            $scope.teamData = angular.copy(selectedTeam);
+
+            $scope.saveField = function($event) {
+                var $element = angular.element($event.target);
+                var $model = $element.controller('ngModel');
+                var $name = $model.$name;
+                var $value = $model.$modelValue;
+
+                selectedTeam[$name] = $value;
+                // TOOD: send to server
+            };
         }
     };
 });
