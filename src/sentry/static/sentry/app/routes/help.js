a0d2090c2d51b51bc68a5cef5a0283a948a18362
David Cramer <dcramer@gmail.com>
2014-03-06 22:00:38 -0800

    minor cleanup to help routes

--- src/sentry/static/sentry/app/routes/help.js
+++ src/sentry/static/sentry/app/routes/help.js
@@ -2,15 +2,9 @@ define(['app'], function() {
     'use strict';
 
     return {
-        url: '/help/',
-        templateUrl: '/_static/sentry/app/templates/help.html',
-        controller: function(teamList, $scope){
-            $scope.teamList = teamList.data;
-        },
-        resolve: {
-            teamList: function($http, $stateParams) {
-              return $http.get('/api/0/teams/');
-            }
-        }
+        parent: 'index',
+        url: 'help/',
+        templateUrl: 'partials/help.html',
+        controller: function($scope){}
     };
 });
--- src/sentry/static/sentry/app/routes/helpGuide.js
+++ src/sentry/static/sentry/app/routes/helpGuide.js
@@ -4,23 +4,15 @@ define(['app'], function() {
     return {
         parent: 'help',
         url: 'guide/:language/',
-        templateUrl: '/_static/sentry/app/templates/help-guide.html',
-        controller: function(teamList, $scope, languageInfo){
-            $scope.teamList = teamList.data;
-            $scope.languageInfo = languageInfo;
-        },
-        resolve: {
-            teamList: function($http, $stateParams) {
-              return $http.get('/api/0/teams/');
-            },
-            languageInfo: function($http, $stateParams) {
-                return {'install': 'pip install raven', 
-                        'initialize': "from raven import Client\n\nclient = Client('{% if dsn %}{% else %}SENTRY_DSN{% endif %}')",
-                        'simpleMessage': "Record a simple message\nclient.captureMessage('hello world!')\n\n# Capture an exception\ntry:\n\t1 / 0\nexcept ZeroDivisionError:\n\tclient.captureException()",
-                        'middleware': 'from raven.middleware import Sentry\n\napplication = Sentry(application, client=client)',
-                        'testConnection': 'raven test {% if dsn %}{{ dsn }}{% else %}SENTRY_DSN{% endif %}'
-                    };
-            }
+        templateUrl: 'partials/help-guide.html',
+        controller: function($scope){
+            $scope.languageInfo = {
+                'install': 'pip install raven',
+                'initialize': "from raven import Client\n\nclient = Client('{% if dsn %}{% else %}SENTRY_DSN{% endif %}')",
+                'simpleMessage': "Record a simple message\nclient.captureMessage('hello world!')\n\n# Capture an exception\ntry:\n\t1 / 0\nexcept ZeroDivisionError:\n\tclient.captureException()",
+                'middleware': 'from raven.middleware import Sentry\n\napplication = Sentry(application, client=client)',
+                'testConnection': 'raven test {% if dsn %}{{ dsn }}{% else %}SENTRY_DSN{% endif %}'
+            };
         }
     };
 });
