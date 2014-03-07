4f7e395ca33b1789dd489800338fb04efd5bad8a
David Cramer <dcramer@gmail.com>
2014-03-07 12:30:20 -0800

    Pull user data into team header

--- src/sentry/static/sentry/app/routes/index.js
+++ src/sentry/static/sentry/app/routes/index.js
@@ -4,12 +4,16 @@ define(['app'], function() {
     return {
         url: '/',
         templateUrl: 'partials/index.html',
-        controller: function(teamList, $scope){
+        controller: function(userData, teamList, $scope){
+            $scope.userData = userData.data;
             $scope.teamList = teamList.data;
         },
         resolve: {
-            teamList: function($http, $stateParams) {
-              return $http.get('/api/0/teams/');
+            userData: function($http) {
+                return $http.get('/api/0/users/me/');
+            },
+            teamList: function(userData) {
+                return {data: userData.data.teams};
             }
         }
     };
