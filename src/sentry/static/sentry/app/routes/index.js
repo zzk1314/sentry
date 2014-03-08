df4f322f81608fe7a6bb2f9a23f49eff05fdceda
David Cramer <dcramer@gmail.com>
2014-03-07 18:07:34 -0800

    Remove redirect as it doesnt behave as expected

--- src/sentry/static/sentry/app/routes/index.js
+++ src/sentry/static/sentry/app/routes/index.js
@@ -7,9 +7,10 @@ define(['app'], function() {
         controller: function(userData, teamList, $state, $scope){
             $scope.userData = userData.data;
             $scope.teamList = teamList.data;
-            if ($scope.teamList.length == 1) {
-                $state.go('team', {team_slug: $scope.teamList[0].slug});
-            }
+            // TODO(dcramer): figure out how to do this without always forcing it
+            // if ($scope.teamList.length == 1) {
+            //     $state.go('team', {team_slug: $scope.teamList[0].slug});
+            // }
         },
         resolve: {
             userData: function($http) {
