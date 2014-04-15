ab4fc1f909fd3133e5a7aee7e438aa8f5f448b75
David Cramer <dcramer@gmail.com>
2014-04-14 22:02:18 -0700

    Add interface data as entries in event_details

--- src/sentry/api/endpoints/event_details.py
+++ src/sentry/api/endpoints/event_details.py
@@ -4,15 +4,31 @@ from sentry.api.base import Endpoint
 from sentry.api.permissions import assert_perm
 from sentry.api.serializers import serialize
 from sentry.models import Event
+from sentry.web.helpers import group_is_public
 
 
 class EventDetailsEndpoint(Endpoint):
+    def _get_entries(self, request, event):
+        # XXX(dcramer): These are called entries for future-proofing
+        is_public = group_is_public(event.group, request.user)
+
+        interface_list = []
+        for interface in event.interfaces.itervalues():
+            # TODO(dcramer): this should be a method in the interface API so permissions
+            # can be passed
+            interface_list.append((interface, interface.serialize()))
+        interface_list.sort(key=lambda x: x[0].get_display_score(), reverse=True)
+
+        return [i[1] for i in interface_list]
+
     def get(self, request, event_id):
         event = Event.objects.get(
             id=event_id
         )
 
         assert_perm(event, request.user)
+
+        Event.objects.bind_nodes([event], 'data')
 
         base_qs = Event.objects.filter(
             group=event.group_id,
@@ -37,5 +53,7 @@ class EventDetailsEndpoint(Endpoint):
             data['previousEventID'] = str(prev_event.id)
         else:
             data['previousEventID'] = None
+
+        data['entries'] = self._get_entries(request, event)
 
         return Response(data)
