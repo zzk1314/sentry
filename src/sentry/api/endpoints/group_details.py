7f32dc26d953d7e18ad9456774763b35ade91539
David Cramer <dcramer@gmail.com>
2014-04-14 21:35:48 -0700

    Add activity and seenBy to group details

--- src/sentry/api/endpoints/group_details.py
+++ src/sentry/api/endpoints/group_details.py
@@ -3,10 +3,45 @@ from rest_framework.response import Response
 from sentry.api.base import Endpoint
 from sentry.api.permissions import assert_perm
 from sentry.api.serializers import serialize
-from sentry.models import Group
+from sentry.models import Activity, Group, GroupSeen
 
 
 class GroupDetailsEndpoint(Endpoint):
+    def _get_activity(self, request, group, num=7):
+        activity_items = set()
+        activity = []
+        activity_qs = Activity.objects.filter(
+            group=group,
+        ).order_by('-datetime').select_related('user')
+        # we select excess so we can filter dupes
+        for item in activity_qs[:num * 2]:
+            sig = (item.event_id, item.type, item.ident, item.user_id)
+            # TODO: we could just generate a signature (hash(text)) for notes
+            # so theres no special casing
+            if item.type == Activity.NOTE:
+                activity.append(item)
+            elif sig not in activity_items:
+                activity_items.add(sig)
+                activity.append(item)
+
+        activity.append(Activity(
+            project=group.project,
+            group=group,
+            type=Activity.FIRST_SEEN,
+            datetime=group.first_seen,
+        ))
+
+        return activity[:num]
+
+    def _get_seen_by(self, request, group):
+        seen_by = sorted(filter(lambda ls: ls[0] != request.user and ls[0].email, [
+            (gs.user, gs.last_seen)
+            for gs in GroupSeen.objects.filter(
+                group=group
+            ).select_related('user')
+        ]), key=lambda ls: ls[1], reverse=True)
+        return seen_by
+
     def get(self, request, group_id):
         group = Group.objects.get(
             id=group_id,
@@ -15,5 +50,14 @@ class GroupDetailsEndpoint(Endpoint):
         assert_perm(group, request.user)
 
         data = serialize(group, request.user)
+
+        # TODO: these probably should be another endpoint
+        activity = self._get_activity(request, group, num=7)
+        seen_by = self._get_seen_by(request, group)
+
+        data.update({
+            'activity': serialize(activity, request.user),
+            'seenBy': serialize(seen_by, request.user),
+        })
 
         return Response(data)
