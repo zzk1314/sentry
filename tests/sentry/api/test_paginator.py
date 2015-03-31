9375cad5d73ea5550f205f3a30121921846275b4
David Cramer <dcramer@gmail.com>
2015-03-31 12:57:36 -0700

    Add tests to exhibit failing cursor behavior

---
+++ tests/sentry/api/test_paginator.py
@@ -0,0 +1,73 @@
+from __future__ import absolute_import
+
+from django.utils import timezone
+
+from sentry.api.paginator import Paginator
+from sentry.models import Group
+from sentry.testutils import TestCase
+
+
+class PaginatorTest(TestCase):
+    def setUp(self):
+        now = timezone.now()
+        project = self.project
+
+        self.group1 = self.create_group(
+            checksum='a' * 32,
+            last_seen=now,
+        )
+        self.group2 = self.create_group(
+            checksum='b' * 32,
+            last_seen=now,
+        )
+        self.queryset = Group.objects.filter(project=project)
+
+    def test_unique_sort_key(self):
+        paginator = Paginator(self.queryset, order_by='id')
+
+        result = paginator.get_result(limit=1)
+        assert len(result) == 1
+        assert result[0].id == self.group1.id
+
+        result = paginator.get_result(limit=1, cursor=result.next)
+        assert len(result) == 1
+        assert result[0].id == self.group2.id
+
+        result = paginator.get_result(limit=1, cursor=result.next)
+        assert len(result) == 0
+
+        result = paginator.get_result(limit=1, cursor=result.prev)
+        assert len(result) == 1
+        assert result[0].id == self.group2.id
+
+        result = paginator.get_result(limit=1, cursor=result.prev)
+        assert len(result) == 1
+        assert result[0].id == self.group1.id
+
+        result = paginator.get_result(limit=1, cursor=result.prev)
+        assert len(result) == 0
+
+    def test_duplicate_sort_key(self):
+        paginator = Paginator(self.queryset, order_by='last_seen')
+
+        result = paginator.get_result(limit=1)
+        assert len(result) == 1
+        assert result[0].id == self.group1.id
+
+        result = paginator.get_result(limit=1, cursor=result.next)
+        assert len(result) == 1
+        assert result[0].id == self.group2.id
+
+        result = paginator.get_result(limit=1, cursor=result.next)
+        assert len(result) == 0
+
+        result = paginator.get_result(limit=1, cursor=result.prev)
+        assert len(result) == 1
+        assert result[0].id == self.group2.id
+
+        result = paginator.get_result(limit=1, cursor=result.prev)
+        assert len(result) == 1
+        assert result[0].id == self.group1.id
+
+        result = paginator.get_result(limit=1, cursor=result.prev)
+        assert len(result) == 0
