18a948643f1827693a14a543087da415f3bcfc71
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-04-17 13:44:33 -0700

    Remove services that have been replaced by docker-compose

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Make sure to always start all the background services
-service postgresql start
-service redis-server start
-service memcached start
-service postfix start
-service ntp start
-
 if [ ! -f /.bootstrapped ]; then
   SENTRY_LIGHT_BUILD=1 pip install -vvv -e .[dev,tests]
   npm install
