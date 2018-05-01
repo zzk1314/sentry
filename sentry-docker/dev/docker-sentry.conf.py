802043df16fe0a573b3336080b69e65aac2c220c
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-04-30 17:51:00 -0700

    Update with new configs for sentry

--- sentry-docker/dev/docker-sentry.conf.py
+++ sentry-docker/dev/docker-sentry.conf.py
@@ -5,13 +5,11 @@ import os.path
 
 CONF_ROOT = os.path.dirname(__file__)
 
-DEBUG = True
-
 # environment variables that would be set by a docker link
-REDIS_ADDR = os.environ['REDIS_PORT_6379_TCP_ADDR']
-REDIS_PORT = int(os.environ['REDIS_PORT_6379_TCP_PORT'])
-POSTGRES_ADDR = os.environ['DB_PORT_5432_TCP_ADDR']
-POSTGRES_PORT = int(os.environ['DB_PORT_5432_TCP_PORT'])
+REDIS_ADDR = "redis"
+REDIS_PORT = 6379
+POSTGRES_ADDR = "db"
+POSTGRES_PORT = 5432
 
 DATABASES = {
     'default': {
@@ -19,10 +17,29 @@ DATABASES = {
         'NAME': 'sentry',
         'USER': 'sentry',
         'PASSWORD': '',
-        'HOST': POSTGRES_ADDR,
-        'PORT': POSTGRES_PORT,
+        'HOST': '',
+        'PORT': '',
+        'AUTOCOMMIT': True,
+        'ATOMIC_REQUESTS': False,
     }
 }
+
+# You should not change this setting after your database has been created
+# unless you have altered all schemas first
+SENTRY_USE_BIG_INTS = True
+
+# If you're expecting any kind of real traffic on Sentry, we highly recommend
+# configuring the CACHES and Redis settings
+
+###########
+# General #
+###########
+
+# Instruct Sentry that this install intends to be run by a single organization
+# and thus various UI optimizations should be enabled.
+SENTRY_SINGLE_ORGANIZATION = True
+# DEBUG = %(debug_flag)s
+
 #########
 # Redis #
 #########
@@ -57,5 +74,86 @@ SENTRY_REDIS_OPTIONS = {
 #         'LOCATION': ['127.0.0.1:11211'],
 #     }
 # }
+
+
+# A primary cache is required for things such as processing events
+SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
+
+#########
+# Queue #
+#########
+
+# See https://docs.sentry.io/on-premise/server/queue/ for more
+# information on configuring your queue broker and workers. Sentry relies
+# on a Python framework called Celery to manage queues.
+
+BROKER_URL = 'redis://localhost:6379'
+
+###############
+# Rate Limits #
+###############
+
+# Rate limits apply to notification handlers and are enforced per-project
+# automatically.
+
+SENTRY_RATELIMITER = 'sentry.ratelimits.redis.RedisRateLimiter'
+
+##################
+# Update Buffers #
+##################
+
+# Buffers (combined with queueing) act as an intermediate layer between the
+# database and the storage API. They will greatly improve efficiency on large
+# numbers of the same events being sent to the API in a short amount of time.
+# (read: if you send any kind of real data to Sentry, you should enable buffers)
+
+SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
+
+##########
+# Quotas #
+##########
+
+# Quotas allow you to rate limit individual projects or the Sentry install as
+# a whole.
+
+SENTRY_QUOTAS = 'sentry.quotas.redis.RedisQuota'
+
+########
+# TSDB #
+########
+
+# The TSDB is used for building charts as well as making things like per-rate
+# alerts possible.
+
+SENTRY_TSDB = 'sentry.tsdb.redis.RedisTSDB'
+
+###########
+# Digests #
+###########
+
+# The digest backend powers notification summaries.
+
+SENTRY_DIGESTS = 'sentry.digests.backends.redis.RedisBackend'
+
+##############
+# Web Server #
+##############
+
+# If you're using a reverse SSL proxy, you should enable the X-Forwarded-Proto
+# header and uncomment the following settings
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+
+# If you're not hosting at the root of your web server,
+# you need to uncomment and set it to the path where Sentry is hosted.
+# FORCE_SCRIPT_NAME = '/sentry'
+
+SENTRY_WEB_HOST = '0.0.0.0'
+SENTRY_WEB_PORT = 9000
+SENTRY_WEB_OPTIONS = {
+    # 'workers': 3,  # the number of web workers
+    # 'protocol': 'uwsgi',  # Enable uwsgi protocol instead of http
+}
 
 SECRET_KEY = 'DOCKER_SECRET_KEY_NOT_REALLY_SECRET'
