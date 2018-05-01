6e099e357317db5395f9b7a757b9536613c46a57
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-04-30 22:55:27 -0700

    Update script to copy files after sentry init run

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,6 +5,9 @@ if [ ! -f /.bootstrapped ]; then
   SENTRY_LIGHT_BUILD=1 pip install -vvv -e .[dev,tests]
   yarn install
   sentry init $SENTRY_CONF
+  # copy over init files with docker-specific setup
+  cp sentry-docker/dev/docker-sentry.conf.py $SENTRY_CONF/sentry.conf.py
+  cp sentry-docker/dev/docker-config.yml $SENTRY_CONF/config.yml
   sentry upgrade --noinput
   sentry createuser --email=root@localhost --password=admin --superuser --no-input
   touch /.bootstrapped
