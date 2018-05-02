976e9e2e5666d11494a3e794d4b0eff3587b218a
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-05-02 13:17:34 -0700

    Replace db environment variables
    After removed, the build broke

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,17 +2,20 @@
 set -ex
 
 if [ ! -f /.bootstrapped ]; then
-  SENTRY_LIGHT_BUILD=1 pip install -vvv -e .[dev,tests]
-  yarn install
-  sentry init $SENTRY_CONF
-  # copy over init files with docker-specific setup
-  cp sentry-docker/dev/docker-sentry.conf.py $SENTRY_CONF/sentry.conf.py
-  cp sentry-docker/dev/docker-config.yml $SENTRY_CONF/config.yml
-  sentry upgrade --noinput
-  sentry createuser --email=root@localhost --password=admin --superuser --no-input
-  touch /.bootstrapped
-
-  echo "done" # && exit 0
+    SENTRY_LIGHT_BUILD=1 pip install -vvv -e .[dev,tests]
+    yarn install
+    touch /.bootstrapped
+    echo "bootstrapped!"
+fi
+if [ ! -f /.bootstrapped-sentry ]; then
+    sentry init $"SENTRY_CONF"
+    # copy over init files with docker-specific setup
+    cp sentry-docker/dev/docker-sentry.conf.py $"SENTRY_CONF"/sentry.conf.py
+    cp sentry-docker/dev/docker-config.yml $"SENTRY_CONF"/config.yml
+    sentry upgrade --noinput
+    sentry createuser --email=root@localhost --password=admin --superuser --no-input
+    echo "sentry success"
+    echo "done" # && exit 0
 fi
 
 exec "$@"
