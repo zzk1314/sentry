9d411777ecdc8775bf601723c7669abc12fde897
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-05-03 16:03:38 -0700

    Remove entrypoint debugging edits

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -7,14 +7,13 @@ if [ ! -f /.bootstrapped ]; then
     touch /.bootstrapped
     echo "bootstrapped!"
 fi
+
 if [ ! -f /.bootstrapped-sentry ]; then
-    sentry init $"SENTRY_CONF"
-    # copy over init files with docker-specific setup
-    cp sentry-docker/dev/docker-sentry.conf.py $"SENTRY_CONF"/sentry.conf.py
-    cp sentry-docker/dev/docker-config.yml $"SENTRY_CONF"/config.yml
+
     sentry upgrade --noinput
     sentry createuser --email=root@localhost --password=admin --superuser --no-input
     echo "sentry success"
+    touch /.bootstrapped-sentry
     echo "done" # && exit 0
 fi
 
