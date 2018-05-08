a71561f5f36bb571d6a63c8701bf06a5b4ff0151
Elaine Yeung <31430673+egsy@users.noreply.github.com>
2018-05-07 18:43:45 -0700

    Update script to include make develop and clearer installation output

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 set -ex
 
-if [ ! -f /.bootstrapped ]; then
+if [ ! -f /.bootstrapped-lib ]; then
     SENTRY_LIGHT_BUILD=1 pip install -vvv -e .[dev,tests]
     yarn install
     touch /.bootstrapped
-    echo "bootstrapped!"
+    make develop
+    echo "SUCCESS: Libraries and dependencies installed!"
 fi
 
 if [ ! -f /.bootstrapped-sentry ]; then
-
     sentry upgrade --noinput
     sentry createuser --email=root@localhost --password=admin --superuser --no-input
-    echo "sentry success"
+    echo "User created
+
+    login: root@localhost
+    password: admin
+
+
+    "
     touch /.bootstrapped-sentry
-    echo "done" # && exit 0
+    echo "SUCCESS: Sentry development environment set up" && exit 0
 fi
 
 exec "$@"
