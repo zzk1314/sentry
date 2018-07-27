bdab1cfce11f3a387489a7b0306e4acaf8c07243
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-27 15:19:59 -0700

    fix(forms): Correct ref and prop passing for textareas

--- src/sentry/static/sentry/app/views/settings/components/forms/controls/textarea.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/controls/textarea.jsx
@@ -2,25 +2,30 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import TextareaAutosize from 'react-autosize-textarea';
 import styled from 'react-emotion';
+import isPropValid from '@emotion/is-prop-valid';
 
 import {inputStyles} from 'app/styles/input';
 
-const TextArea = styled(
-  p =>
-    p.autosize ? (
-      <TextareaAutosize rows={p.rows ? p.rows : 2} async={true} {...p} />
+const TextAreaControl = React.forwardRef(
+  ({autosize, ...p}, ref) =>
+    autosize ? (
+      <TextareaAutosize async innerRef={ref} rows={p.rows ? p.rows : 2} {...p} />
     ) : (
-      <textarea {...p} />
+      <texarea ref={ref} {...p} />
     )
-)`
-  ${inputStyles};
-`;
+);
 
-TextArea.propTypes = {
+TextAreaControl.propTypes = {
   /**
    * Enable autosizing of the textarea.
    */
   autosize: PropTypes.bool,
 };
+
+const propFilter = p => ['autosize', 'rows', 'maxRows'].includes(p) || isPropValid(p);
+
+const TextArea = styled(TextAreaControl, {shouldForwardProp: propFilter})`
+  ${inputStyles};
+`;
 
 export default TextArea;
