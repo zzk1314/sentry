af69a72656bf368bad0469277105482d5b3ab713
Billy Vong <billy@sentry.io>
2018-08-15 18:23:02 -0700

    clean Browsers view

--- src/sentry/static/sentry/app/views/organizationHealth/browsers.jsx
+++ src/sentry/static/sentry/app/views/organizationHealth/browsers.jsx
@@ -1,25 +1,17 @@
-import {Box, Flex} from 'grid-emotion';
-import PropTypes from 'prop-types';
+import {Flex} from 'grid-emotion';
 import React from 'react';
 import styled from 'react-emotion';
 
-import {Panel, PanelHeader, PanelBody, PanelItem} from 'app/components/panels';
-import {TableChart} from 'app/components/charts/tableChart';
 import {t} from 'app/locale';
-import AreaChart from 'app/components/charts/areaChart';
-import Count from 'app/components/count';
-import PercentageBarChart from 'app/components/charts/percentageBarChart';
-import IdBadge from 'app/components/idBadge';
 import PanelChart from 'app/components/charts/panelChart';
 import PieChart from 'app/components/charts/pieChart';
-import overflowEllipsis from 'app/styles/overflowEllipsis';
 import space from 'app/styles/space';
-import theme from 'app/utils/theme';
 import withApi from 'app/utils/withApi';
 import withLatestContext from 'app/utils/withLatestContext';
 
 import HealthContext from './util/healthContext';
 import HealthRequest from './util/healthRequest';
+import EventsTableChart from './eventsTableChart';
 
 const OrganizationHealthErrors = styled(
   class extends React.Component {
@@ -46,13 +38,7 @@ const OrganizationHealthErrors = styled(
                     ]}
                     title={t('Browsers')}
                   >
-                    {({series}) => (
-                      <Flex>
-                        <PieChartWrapper>
-                          <PieChart height={300} series={series} />
-                        </PieChartWrapper>
-                      </Flex>
-                    )}
+                    {({series}) => <PieChart height={300} series={series} />}
                   </StyledPanelChart>
                 );
               }}
@@ -71,66 +57,7 @@ const OrganizationHealthErrors = styled(
                     ]}
                     title={t('OS')}
                   >
-                    {({series}) => (
-                      <Flex>
-                        <PieChartWrapper>
-                          <PieChart height={300} series={series} />
-                        </PieChartWrapper>
-                      </Flex>
-                    )}
-                  </StyledPanelChart>
-                );
-              }}
-            </HealthRequest>
-          </Flex>
-
-          <Flex>
-            <HealthRequest tag="device" timeseries={false}>
-              {({data, loading}) => {
-                if (!data) return null;
-                return (
-                  <StyledPanelChart
-                    height={200}
-                    series={[
-                      {
-                        seriesName: t('Browsers'),
-                        data: data.map(([name, value]) => ({name, value})),
-                      },
-                    ]}
-                    title={t('Browsers')}
-                  >
-                    {({series}) => (
-                      <Flex>
-                        <PieChartWrapper>
-                          <PieChart height={300} series={series} />
-                        </PieChartWrapper>
-                      </Flex>
-                    )}
-                  </StyledPanelChart>
-                );
-              }}
-            </HealthRequest>
-            <HealthRequest tag="device.family" timeseries={false}>
-              {({data, loading}) => {
-                if (!data) return null;
-                return (
-                  <StyledPanelChart
-                    height={200}
-                    series={[
-                      {
-                        seriesName: t('OS'),
-                        data: data.map(([name, value]) => ({name, value})),
-                      },
-                    ]}
-                    title={t('OS')}
-                  >
-                    {({series}) => (
-                      <Flex>
-                        <PieChartWrapper>
-                          <PieChart height={300} series={series} />
-                        </PieChartWrapper>
-                      </Flex>
-                    )}
+                    {({series}) => <PieChart height={300} series={series} />}
                   </StyledPanelChart>
                 );
               }}
@@ -139,7 +66,7 @@ const OrganizationHealthErrors = styled(
 
           <Flex>
             <HealthRequest tag="browser.name" timeseries={false} topk={5}>
-              {({data, originalData, loading}) => {
+              {({data, originalData, tag, loading}) => {
                 if (!data) return null;
 
                 const total = data.reduce((acc, [, value]) => acc + value, 0);
@@ -153,78 +80,20 @@ const OrganizationHealthErrors = styled(
                     {}
                   );
                 return (
-                  <React.Fragment>
-                    {!loading && (
-                      <Panel style={{width: '100%'}}>
-                        <PanelHeader>
-                          <Flex
-                            flex={1}
-                            style={{flexShrink: 0, marginRight: 18}}
-                            justify="space-between"
-                            align="center"
-                          >
-                            <span>Browser</span>
-                            <span>Events</span>
-                          </Flex>
-                          <Flex flex={3} justify="space-between" align="center">
-                            <span>Percentage</span>
-                            <span>Last Event</span>
-                          </Flex>
-                        </PanelHeader>
-                        <PanelBody>
-                          {originalData.map(
-                            ({count, lastCount, ['browser.name']: name}) => {
-                              return (
-                                <PanelItem key={name}>
-                                  <Flex
-                                    flex={1}
-                                    style={{flexShrink: 0, marginRight: 18}}
-                                    justify="space-between"
-                                    align="center"
-                                  >
-                                    <span>{name}</span>
-                                    <span>
-                                      {count}
-
-                                      {` (${count > lastCount
-                                        ? '+'
-                                        : count === lastCount ? '' : '-'}${Math.round(
-                                        Math.abs(count - lastCount) / (count + lastCount)
-                                      )}%)`}
-                                    </span>
-                                  </Flex>
-                                  <Flex flex={3} justify="space-between" align="center">
-                                    <Flex w={[3 / 4]} align="center">
-                                      <div
-                                        style={{
-                                          width: '85%',
-                                          marginRight: 6,
-                                        }}
-                                      >
-                                        <div
-                                          style={{
-                                            flex: 1,
-                                            width: `${seriesPercentages[name]}%`,
-                                            backgroundColor: '#ccc',
-                                            height: 12,
-                                            borderRadius: 2,
-                                          }}
-                                        />
-                                      </div>
-                                      <span>{seriesPercentages[name]}%</span>
-                                    </Flex>
-                                    <Flex w={[1 / 4]} justify="flex-end">
-                                      <span>5 minutes ago</span>
-                                    </Flex>
-                                  </Flex>
-                                </PanelItem>
-                              );
-                            }
-                          )}
-                        </PanelBody>
-                      </Panel>
-                    )}
-                  </React.Fragment>
+                  <EventsTableChart
+                    headers={[
+                      t('Browser'),
+                      t('Events'),
+                      t('Percentage'),
+                      t('Last event'),
+                    ]}
+                    data={originalData.map(({count, lastCount, [tag]: name}) => ({
+                      count,
+                      lastCount,
+                      name,
+                      percentage: seriesPercentages[name],
+                    }))}
+                  />
                 );
               }}
             </HealthRequest>
@@ -237,11 +106,6 @@ const OrganizationHealthErrors = styled(
   }
 )``;
 
-const PieChartWrapper = styled(Box)`
-  flex: 1;
-  flex-shrink: 0;
-  width: 100%;
-`;
 class OrganizationHealthErrorsContainer extends React.Component {
   render() {
     // Destructure props from `withLatestContext`
@@ -274,11 +138,6 @@ const Header = styled(Flex)`
   margin-bottom: ${space(2)};
 `;
 
-const SubduedCount = styled('span')`
-  color: ${p => p.theme.gray1};
-  margin-left: ${space(0.5)};
-`;
-
 const getChartMargin = () => `
   margin-right: ${space(2)};
   &:last-child {
@@ -290,19 +149,4 @@ const StyledPanelChart = styled(PanelChart)`
   ${getChartMargin};
   flex-shrink: 0;
   overflow: hidden;
-`;
-
-const StyledTableChart = styled(TableChart)`
-  ${getChartMargin};
-  flex-shrink: 0;
-  overflow: hidden;
-`;
-
-const ReleaseName = styled(Box)`
-  ${overflowEllipsis};
-`;
-
-const Project = styled(Box)`
-  margin-left: ${space(1)};
-  flex-shrink: 0;
 `;
